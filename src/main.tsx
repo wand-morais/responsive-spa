ae3b8bb111aa1ddc4c455506d13fc75d193804b0
Wanderson Antunes <wanderson_antunes@cp3tecnologia.com.br>
2026-01-26 21:23:20 -0300

    chore: setting up styles file configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import "./styles/global.css";
 
 const container = document.getElementById("root");
 if (!container) throw new Error("Root elment #root not found.");
