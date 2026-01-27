fc2dd4296e3bba75ac862a780ddfd521c633667d
Wanderson Antunes <wanderson_antunes@cp3tecnologia.com.br>
2026-01-27 20:27:03 -0300

    chore: organisation du fichier princiapal et du fichier de configuration Vite

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+
 import "./styles/global.css";
+
+import { App } from "./App";
 
 const container = document.getElementById("root");
 if (!container) throw new Error("Root elment #root not found.");
 
-createRoot(container).render(<React.StrictMode> </React.StrictMode>);
+createRoot(container).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
